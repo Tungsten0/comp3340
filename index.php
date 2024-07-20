6f337c18140f426dd60bfa02fd1ab407ca3e95b7
Marco <115311708+Tungsten0@users.noreply.github.com>
2024-07-19 22:46:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,13 +11,5 @@ if (isset($_COOKIE['user_id'])) {
 } else {
   // The user_id cookie has not been set
   echo "User ID cookie is not set.";
-  switch ($request) {
-    case '/login' :
-      require __DIR__ . '/pages/login.php';
-      break;
-    default:
-      http_response_code(404);
-      require __DIR__ . '/pages/404.php';
-      break;
-  }
+  require __DIR__ . '/pages/login.php';
 }
