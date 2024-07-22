a5008ac4a1a87f7c4dbacd5a367e350422d9ed39
Marco <115311708+Tungsten0@users.noreply.github.com>
2024-07-22 03:04:52 -0400

    add user php script added and updated

---
+++ php/add_user.php
@@ -0,0 +1,41 @@
+<?
+
+#add user form from admin dashboard
+session_start();
+include '../config/db_connection.php';
+
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $permission = $_POST['permissions'];
+
+    //prevent sql injection
+    $fname = stripcslashes($fname);
+    $lname = stripcslashes($lname);
+    $username = stripcslashes($username);
+    $email = stripcslashes($email);
+    $password = stripcslashes($password);
+    $fname = mysqli_real_escape_string($conn, $fname);
+    $lname = mysqli_real_escape_string($conn, $lname);
+    $username = mysqli_real_escape_string($conn, $username);
+    $email = mysqli_real_escape_string($conn, $email);
+    $password = mysqli_real_escape_string($conn, $password);
+
+    //hash password
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (fname, lname, username, email, password, permissions) VALUES ('$fname', '$lname', '$username', '$email', '$password', '$permission')";
+    $result = $conn->query($sql);
+
+    if ($result) {
+        echo "User added successfully.";
+        header('Location: /pages/Add_User.html');
+    } else {
+        echo "User add failed.";
+        header('Location: /pages/Add_User.html#error');
+    }
+}
