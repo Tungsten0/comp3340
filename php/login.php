22c8c9efb2daf01673cd77144cce01ccc3a22bc4
Marco <115311708+Tungsten0@users.noreply.github.com>
2024-07-22 18:51:00 -0400

    Update login.php

--- php/login.php
+++ php/login.php
@@ -6,6 +6,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    echo "Username: " . $username . "<br>";
+    echo "Password: " . $password . "<br>";
+
     //prevent sql injection
     $username = stripcslashes($username);
     $password = stripcslashes($password);
