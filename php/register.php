ee2d8e03c40dc485056487957496aabb79943d10
Marco <115311708+Tungsten0@users.noreply.github.com>
2024-07-22 02:44:17 -0400

    css restructure
    organized and restructured all css into individual css files

---
+++ php/register.php
@@ -0,0 +1,26 @@
+<?
+#register php script
+
+if (isset($_POST['register'])) {
+    include '../config/db_connection.php';
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    //prevent sql injection
+    $email = stripcslashes($email);
+    $password = stripcslashes($password);
+    $email = mysqli_real_escape_string($conn, $email);
+    $password = mysqli_real_escape_string($conn, $password);
+
+    $sql = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
+    $result = $conn->query($sql);
+
+    if ($result) {
+        echo "Registration successful.";
+        header('Location: /pages/login.html');
+    } else {
+        echo "Registration failed.";
+        header('Location: /pages/register.html#error');
+    }
+}
