2a3f8477e487bce249661d5bf5acd01a3e7a139b
Marco <115311708+Tungsten0@users.noreply.github.com>
2024-07-23 03:31:34 -0400

    registration code initial

--- php/register.php
+++ php/register.php
@@ -4,23 +4,54 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     include '../config/db_connection.php';
 
-    $email = $_POST['email'];
+    $username = $_POST['username'];
     $password = $_POST['password'];
+    $email = $_POST['email'];
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
 
     //prevent sql injection
+    $username = stripcslashes($username);
+    $password = stripcslashes($password);
     $email = stripcslashes($email);
     $password = stripcslashes($password);
+    $fname = stripcslashes($fname);
+    $lname = stripcslashes($lname);
+    $username = mysqli_real_escape_string($conn, $username);
+    $password = mysqli_real_escape_string($conn, $password);
     $email = mysqli_real_escape_string($conn, $email);
     $password = mysqli_real_escape_string($conn, $password);
+    $fname = mysqli_real_escape_string($conn, $fname);
+    $lname = mysqli_real_escape_string($conn, $lname);
 
-    $sql = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
-    $result = $conn->query($sql);
+    //check if username already exists
+    $check_username = $conn->prepare("SELECT * FROM users WHERE username = ?");
+    $check_username->bind_param("s", $username);
+    $check_username->execute();
+    $check_username->store_result();
 
-    if ($result) {
-        echo "Registration successful.";
-        header('Location: /pages/login.html');
+    //if username already exists
+    if ($check_username->num_rows > 0) {
+        ?> <script>console.log("Username already exists.");</script> <?php
+        header('Location: /pages/login.html#user_exists');
     } else {
-        echo "Registration failed.";
-        header('Location: /pages/register.html#error');
+        $stmt = $conn->prepare("INSERT INTO registration (username, password, email, first_name, last_name) VALUES (?, ?, ?, ?, ?)");
+
+        if ($stmt) {
+            $password_hashed = password_hash($password, PASSWORD_DEFAULT);
+            $stmt->bind_param("sssss", $username, $password_hashed, $email, $fname, $lname);
+
+            if ($stmt->execute()) {
+                ?> <script>console.log("User added successfully.");</script> <?php
+                header('Location: /pages/login.html#user_added');
+            } else {
+                ?> <script>console.log("User add failed: " . $stmt->error);</script> <?php
+                header('Location: /pages/register.html#add_error');
+            }
+            $stmt->close();
+        } else {
+            ?> <script>console.log("Preparation failed: " . $conn->error);</script> <?php
+            header('Location: /pages/register.html#prep_error');
+        }
     }
 }
